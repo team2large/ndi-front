7106c9b3b5ea034bc1e010d94375760cf0cb7b0e
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-02 00:29:10 +0100

    bug: card clicked doesn't work

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,63 +3,24 @@ import Tilt from 'react-parallax-tilt';
 import dataCondoms from 'components/condoms.json';
 import styles from 'assets/style/card.module.scss';
 
-const Card = () => {
-  const [isClicked, setIsClicked] = useState(false);
-
-  const isNorme = true;
-  const taille = 'XXXL';
-  const date = new Date();
-
+const Card = ({ data, onClick }) => {
   const handleClick = () => {
-    setIsClicked(true);
+    data.clicked = !data.clicked;
+    onClick(data);
   };
-
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-
-  const randomize = (nbrCards) => {
-    const max = dataCondoms.length;
-    const condoms = [];
-    let condom;
-    for (let i = 0;i < nbrCards;i++) {
-      condom = dataCondoms[getRandomInt(max)];
-      condom.isNorme = true;
-      condom.taille = 'L';
-      condom.date = new Date() - getRandomInt(300);
-      switch (getRandomInt(4)) {
-        case 1:
-          condom.date = new Date() - getRandomInt(300);
-          condom.reasonNotValid += 'La date de péremption est passée';
-          break;
-        case 2:
-          condom.taille = 'XXXXXXXL';
-          condom.reasonNotValid += 'La taille est trop grande.';
-          break;
-        case 3:
-          condom.isNorme = true;
-          condom.reasonNotValid += 'Il faut que le préservatif soit conforme à la réglementation européenne';
-          break;
-        default:
-          break;
-      }
-    }
-    condoms.push(condom);
-    console.log(condoms);
-  };
-
-
-  randomize(10);
 
   return (
     <Tilt>
       <div className={styles.card} onClick={handleClick}>
         <div className={styles.card__inner}>
-          <div className={styles.image}></div>
+          <div className={styles.image}>
+            <img src={data.image} alt='condom' />
+          </div>
           <div className={styles.content}>
-            <p>Date de péremption : {date.toLocaleDateString()}</p>
-            <p>Taille : {taille}</p>
-            {isNorme && <p>Conforme à la réglementation européenne</p>}
+            <p>Date de péremption : {data.date.toLocaleDateString()}</p>
+            <p>Taille : {data.size}</p>
+            {data.isNorme && <p>Conforme à la réglementation européenne</p>}
+            <p>Data : {data.clicked ? 'clicked' : 'not'}</p>
           </div>
         </div>
       </div>
--- src/pages/CardsGame.jsx
+++ src/pages/CardsGame.jsx
@@ -1,35 +1,106 @@
 import React, { useEffect, useState } from 'react';
 import Card from 'components/Card';
 import styles from 'assets/style/cardsgame.module.scss';
+import dataCondoms from 'components/condoms.json';
 
 const CardsGame = () => {
-  const ATTEMPT_MAX = 3;
+  // const ATTEMPT_MAX = 3;
 
-  const [score, setScore] = useState(null);
-  const [attempt, setAttempt] = useState(0);
-  const [isWin, setIsWin] = useState(false);
+  // const [score, setScore] = useState(null);
+  // const [attempt, setAttempt] = useState(0);
+  // const [isWin, setIsWin] = useState(false);
+  // const [condoms, setCondoms] = useState([]);
+
+  // const cards = [];
+
+  // useEffect(() => {
+  //   for (let i = 0;i < 9;i++)
+  //     cards.push(<Card />);
+  // }, []);
+
+  // useEffect(() => {
+  //   if (attempt === ATTEMPT_MAX) {
+  //     setIsWin(false);
+  //     setScore(0);
+  //   } else if (isWin)
+  //     setScore(ATTEMPT_MAX - attempt);
+  // }, [attempt]);
+
+  // un state qui contient toutes les cartes en jeu -> 9
+  const [cards, setCards] = useState([]);
+  const NBR_CARDS = 9;
 
   useEffect(() => {
-    if (attempt === ATTEMPT_MAX) {
-      setIsWin(false);
-      setScore(0);
-    } else if (isWin)
-      setScore(ATTEMPT_MAX - attempt);
-  }, [attempt]);
+    for (let i = 0;i < NBR_CARDS;i++)
+      createCard();
+
+    // taf = creer l'array de card
+    setCards([
+      {
+        name: 'Préservatif qu\'il faut',
+        isValid: true,
+        image: '/src/images/condoms/capote_pas_utilise_orange.png',
+        reasonNotValid: 'bonsoir',
+        date: new Date(),
+        size: 'L',
+        isNorme: true,
+        clicked: false,
+      }
+    ]);
+    // faut randomizer
+    // array = propre et prete pour jouer
+    // mettre sa dans le state card
+  }, []);
+
+  const handleCardCliked = (card) => {
+    console.log('Clicked', card);
+    // validite
+    // gerer le jeu
+    // check game ended
+  };
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  const setError = (condom) => {
+    for (let i = 0;i < getRandomInt(2);i++) {
+      switch (getRandomInt(4)) {
+        case 0:
+          condom.date = new Date() - getRandomInt(300);
+          condom.reasonNotValid += 'La date de péremption est passée.';
+          break;
+        case 1:
+          condom.isNorme = true;
+          condom.reasonNotValid += 'Il faut que le préservatif soit conforme à la réglementation européenne';
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  const createCard = () => {
+    const max = dataCondoms.length;
+    const condom = dataCondoms[getRandomInt(max)];
+    condom.isNorme = true;
+    condom.taille = 'L';
+    condom.date = new Date() + getRandomInt(300);
+    if (getRandomInt(2)) {
+      condom.isValid = false;
+      setError(condom);
+    }
+    console.log(condom);
+    return condom;
+  };
 
   return (
     <div className={styles.cards_game}>
       <div className={styles.wrapper}>
         <div className={styles.cards_container}>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {cards.map((card, i) => (
+            <Card data={card} onClick={handleCardCliked} key={i} />
+          ))}
         </div>
       </div>
     </div>
