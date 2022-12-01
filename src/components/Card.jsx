9f0f6fc6661f9c882b6dcfb29d43945db454b457
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-01 23:14:15 +0100

    feat: added api + implement rules

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,59 @@
+import React, { useState } from 'react';
 import Tilt from 'react-parallax-tilt';
+import dataCondoms from 'components/condoms.json';
 import styles from 'assets/style/card.module.scss';
 
 const Card = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
   const isNorme = true;
   const taille = 'XXXL';
   const date = new Date();
 
+  const handleClick = () => {
+    setIsClicked(true);
+  };
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  const randomize = (nbrCards) => {
+    const max = dataCondoms.length;
+    const condoms = [];
+    let condom;
+    for (let i = 0;i < nbrCards;i++) {
+      condom = dataCondoms[getRandomInt(max)];
+      condom.isNorme = true;
+      condom.taille = 'L';
+      condom.date = new Date() - getRandomInt(300);
+      switch (getRandomInt(4)) {
+        case 1:
+          condom.date = new Date() - getRandomInt(300);
+          condom.reasonNotValid += 'La date de péremption est passée';
+          break;
+        case 2:
+          condom.taille = 'XXXXXXXL';
+          condom.reasonNotValid += 'La taille est trop grande.';
+          break;
+        case 3:
+          condom.isNorme = true;
+          condom.reasonNotValid += 'Il faut que le préservatif soit conforme à la réglementation européenne';
+          break;
+        default:
+          break;
+      }
+    }
+    condoms.push(condom);
+    console.log(condoms);
+  };
+
+
+  randomize(10);
+
   return (
     <Tilt>
-      <div className={styles.card}>
+      <div className={styles.card} onClick={handleClick}>
         <div className={styles.card__inner}>
           <div className={styles.image}></div>
           <div className={styles.content}>
--- src/pages/CardsGame.jsx
+++ src/pages/CardsGame.jsx
@@ -1,22 +1,39 @@
+import React, { useEffect, useState } from 'react';
 import Card from 'components/Card';
 import styles from 'assets/style/cardsgame.module.scss';
 
-const CardsGame = () => (
-  <div className={styles.cards_game}>
-    <div className={styles.wrapper}>
-      <div className={styles.cards_container}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+const CardsGame = () => {
+  const ATTEMPT_MAX = 3;
+
+  const [score, setScore] = useState(null);
+  const [attempt, setAttempt] = useState(0);
+  const [isWin, setIsWin] = useState(false);
+
+  useEffect(() => {
+    if (attempt === ATTEMPT_MAX) {
+      setIsWin(false);
+      setScore(0);
+    } else if (isWin)
+      setScore(ATTEMPT_MAX - attempt);
+  }, [attempt]);
+
+  return (
+    <div className={styles.cards_game}>
+      <div className={styles.wrapper}>
+        <div className={styles.cards_container}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CardsGame;
