4ac82322c62b491c227cd388eaabba3a38cc3f72
Lucas M <6555778+IkeYeek@users.noreply.github.com>
2022-12-02 00:51:42 +0100

    Dev lucas ajout regles jeu (#12)
    * Update src/pages/GameRules.jsx
    
    * [Fix] fixed GameRule React's antipattern + error on links. [Misc] removed useless dead-code from gamerule's css
    
    * [Fix] added forgotten new routes for game rules
    
    Co-authored-by: Célian Riboulet <celian.riboulet@gmail.com>
    Co-authored-by: Lucas Ike Marquès <lucas.marques@u-bordeaux.fr>

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Home from 'components/Home';
 import Register from 'components/Register';
 import Game from 'pages/Game';
 import { Route, Routes } from 'react-router-dom';
+import GameRules from './pages/GameRules';
 
 const App = () => (
   <Routes>
     <Route path='/' element={<Home/>} />
     <Route path='/register' element={<Register />}/>
-    <Route path='/game/:gameId' element={<Game />}/>
+    <Route path='/game/:gameId' element={<GameRules />}/>
+    <Route path='/game/:gameId/play' element={<Game />}/>
   </Routes>
 );
 
