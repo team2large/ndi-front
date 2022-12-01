a1d41e4785160adb85592b57a289ed9d66fbe683
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-01 22:09:54 +0100

    feat: added card tilt animation + card info

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,25 @@
+import Tilt from 'react-parallax-tilt';
 import styles from 'assets/style/card.module.scss';
 
-const Card = () => (
-  <div className={styles.card}>
-  </div>
-);
+const Card = () => {
+  const isNorme = true;
+  const taille = 'XXXL';
+  const date = new Date();
+
+  return (
+    <Tilt>
+      <div className={styles.card}>
+        <div className={styles.card__inner}>
+          <div className={styles.image}></div>
+          <div className={styles.content}>
+            <p>Date de péremption : {date.toLocaleDateString()}</p>
+            <p>Taille : {taille}</p>
+            {isNorme && <p>Conforme à la réglementation européenne</p>}
+          </div>
+        </div>
+      </div>
+    </Tilt>
+  );
+};
 
 export default Card;
