6b5451b6f461e1bfd656506d476d02dd193fecfd
Guillaume <93042538+gfroidcourt@users.noreply.github.com>
2022-12-02 05:21:25 +0100

    admin style (#19)
    * begin css
    
    * design of admin page (wrong branch name)
    
    * fix title color

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../api';
 import PseudoBar from '../components/PseudoBar';
+import arrow from 'assets/images/admin/goBackArrow.svg';
 import styles from 'assets/style/Admin.module.scss';
+
 const Admin = () => {
   const [scores, setScores] = useState([]);
 
@@ -27,12 +30,21 @@ const Admin = () => {
       });
   };
 
+
   return (
-    <main className={styles.background}>
+    <div className={styles.background}>
+      <div className={styles.topContent}>
+        <h1>Admin</h1>
+        <h2>Classement de tous les jeux</h2>
+      </div>
       <div className={styles.wrapper}>
         {scores.map((score, index) => <PseudoBar key={index} onClick={() => deleteScore(score.id)} {...score} />)}
       </div>
-    </main>
+      <Link className={styles.bottomContent} to={'/'}>
+        <img src={arrow} />
+        <p>Accueil</p>
+      </Link>
+    </div>
   );
 };
 export default Admin;
