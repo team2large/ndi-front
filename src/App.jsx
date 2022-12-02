c82a70ed444d2743d161b0ea2953475c700dbac2
mannarelli-killian <killian.mannarelli@outlook.fr>
2022-12-02 05:13:28 +0100

    feat : depisteur

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from 'components/Home';
 import Register from 'components/Register';
 import Game from 'pages/Game';
+import DepisteurGame from 'pages/DepisteurGame';
 import { Route, Routes } from 'react-router-dom';
 
 const App = () => (
@@ -8,6 +9,7 @@ const App = () => (
     <Route path='/' element={<Home/>} />
     <Route path='/register' element={<Register />}/>
     <Route path='/game/:gameId' element={<Game />}/>
+    <Route path='/game/Depisteur' element={<DepisteurGame />}/>
   </Routes>
 );
 
---
+++ src/pages/DepisteurGame.jsx
@@ -0,0 +1,212 @@
+
+import { useEffect, useState } from 'react';
+import styles from 'assets/style/depisteurgame.module.scss';
+
+const NBR_OF_BOMB = 10;
+const NBR_OF_ROWS = 8;
+const MAX_LIVES = 6;
+
+const createGrid = () => {
+  const grid = [];
+  for (let i = 0;i < NBR_OF_ROWS;i++) {
+    grid.push([]);
+    for (let j = 0;j < NBR_OF_ROWS;j++) {
+      grid[i].push({
+        id: `${i}-${j}`,
+        value: 0,
+        isRevealed: false,
+        isFlagged: false,
+        isBacteria: false,
+        isVirus: false,
+      });
+    }
+  }
+  setDiseases(grid);
+  setValues(grid);
+  return grid;
+};
+
+
+const setDiseases = (grid) => {
+  let count = 0;
+  while (count < NBR_OF_BOMB) {
+    const randomRow = Math.floor(Math.random() * NBR_OF_ROWS);
+    const randomCol = Math.floor(Math.random() * NBR_OF_ROWS);
+    const randomCell = grid[randomRow][randomCol];
+    if (!randomCell.isBacteria && !randomCell.isVirus) {
+      if (Math.random() < 0.5)
+        randomCell.isBacteria = true;
+      else
+        randomCell.isVirus = true;
+
+      count++;
+    }
+  }
+  return grid;
+};
+
+const getAdjacentCases = (grid, row, col) => {
+  row = parseInt(row);
+  col = parseInt(col);
+
+  const adjacentCases = [];
+  for (let i = (row - 1);i <= (row + 1);i++) {
+    for (let j = (col - 1);j <= (col + 1);j++) {
+      if (i >= 0 && i < NBR_OF_ROWS && j >= 0 && j < NBR_OF_ROWS && !(i === row && j === col))
+        adjacentCases.push(JSON.parse(JSON.stringify(grid[i][j])));
+    }
+  }
+  return adjacentCases;
+};
+
+const setValues = (grid) => {
+  for (let i = 0;i < NBR_OF_ROWS;i++) {
+    for (let j = 0;j < NBR_OF_ROWS;j++) {
+      if (!grid[i][j].isVirus && !grid[i][j].isBacteria) {
+        const adjacentCases = getAdjacentCases(grid, i, j);
+        let value = 0;
+        for (let k = 0;k < adjacentCases.length;k++) {
+          if (adjacentCases[k].isVirus || adjacentCases[k].isBacteria)
+            value++;
+        }
+        grid[i][j].value = value;
+      }
+    }
+  }
+  return grid;
+};
+
+
+const DepisteurGame = () => {
+  const [grid, setGrid] = useState(null);
+  const [lives, setLives] = useState(MAX_LIVES);
+  const [revealedDiseases, setRevealedDiseases] = useState(null);
+
+  const revealCase = (grid, row, col) => {
+    let newGrid = JSON.parse(JSON.stringify(grid));
+    const currentCase = newGrid[row][col];
+    if (currentCase.isRevealed || currentCase.isFlagged)
+      return newGrid;
+    if (currentCase.isBacteria || currentCase.isVirus) {
+      if (currentCase.isVirus)
+        setLives(lives - 3);
+      else
+        setLives(lives - 2);
+    }
+    currentCase.isRevealed = true;
+    newGrid = revealAdjacentCases(newGrid, row, col);
+    setGrid(newGrid);
+  };
+
+  const checkVictory = (grid) => {
+    let count = 0;
+    for (let i = 0;i < NBR_OF_ROWS;i++) {
+      for (let j = 0;j < NBR_OF_ROWS;j++) {
+        if (grid[i][j].isRevealed)
+          count++;
+      }
+    }
+    if (count === (NBR_OF_ROWS * NBR_OF_ROWS) - NBR_OF_BOMB)
+      return true;
+    return false;
+  };
+
+  const checkDeath = () => {
+    if (lives <= 0)
+      return true;
+    return false;
+  };
+
+  const revealAdjacentCases = (grid, row, col) => {
+    let newGrid = JSON.parse(JSON.stringify(grid));
+    const currentCase = newGrid[row][col];
+    if (currentCase.value === 0 && !currentCase.isBacteria && !currentCase.isVirus) {
+      const adjacentCases = getAdjacentCases(JSON.parse(JSON.stringify(newGrid)), row, col);
+      for (let i = 0;i < adjacentCases.length;i++) {
+        const adjacentCase = adjacentCases[i];
+        if (!adjacentCase.isRevealed && !adjacentCase.isFlagged && adjacentCase.isBacteria === false && adjacentCase.isVirus === false) {
+          adjacentCase.isRevealed = true;
+          newGrid[adjacentCase.id.split('-')[0]][adjacentCase.id.split('-')[1]] = adjacentCase;
+          newGrid = revealAdjacentCases(JSON.parse(JSON.stringify(newGrid)), adjacentCase.id.split('-')[0], adjacentCase.id.split('-')[1]);
+        }
+      }
+    }
+    return newGrid;
+  };
+
+
+  useEffect(() => {
+    setGrid(createGrid());
+  }, []);
+
+  useEffect(() => {
+    //if (grid == null)
+    //return;
+    //console.log(grid);
+    //console.log(getAdjacentCases(grid, 3, 3));
+  }, [grid]);
+
+  const handleImage = (item) => {
+    if (item.isRevealed) {
+      if (item.isBacteria)
+        return (<img src='/img/games/depisteur/bacterie.png'></img>);
+      if (item.isVirus)
+        return (<img src='/img/games/depisteur/virus.png'></img>);
+      return item.value === 0 ? undefined : item.value;
+    }
+    if (item.isFlagged)
+      return (<img src='/img/games/depisteur/capote_normale_orange.png'></img>);
+    return undefined;
+  };
+
+  const handleOnClick = (item, i, j) => {
+    if (item.isRevealed)
+      return;
+    revealCase(grid, i, j);
+  };
+
+  const handleOnContextMenu = (event, item, i, j) => {
+    event.preventDefault();
+    const newGrid = JSON.parse(JSON.stringify(grid));
+    if (item.isRevealed)
+      return;
+    newGrid[i][j].isFlagged = !newGrid[i][j].isFlagged;
+    setGrid(newGrid);
+  };
+
+  return (
+    <div>
+      <header>
+        <h1>Depisteur</h1>
+        <div className={styles.container}>
+          <div className={styles.lives}>
+            {lives}
+          </div>
+          <table>
+            <tbody>
+              {grid ? grid.map((line, index) => (
+                <tr key={`line-${index}`}>
+                  {line.map((item, index2) => (
+                    <td key={`elem-${index}-${index2}`} className={item.isRevealed ? styles.isRevealed : styles.isHidden}
+                      onClick={() => {
+                        handleOnClick(item, index, index2);
+                      }}
+                      onContextMenu={(e) => {
+                        handleOnContextMenu(e, item, index, index2);
+                      }}
+                    >
+                      {handleImage(item)}
+                    </td>
+                  ))}
+                </tr>
+              )) : <td>no grid</td>}
+            </tbody>
+          </table>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+
+export default DepisteurGame;
